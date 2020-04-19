fc068729f2203990b900143d225b334c6604533a
Henry Chen <henrychen385@gmail.com>
2020-04-20 02:48:49 +0800

    fixed minor error

--- web/src/routes/apiv1.js
+++ web/src/routes/apiv1.js
@@ -1,9 +1,10 @@
 import { Announcement } from '../dbschemas/announcement';
+import * as db from '../mongoosehelpers'
 
 // HTTP FUNCTIONS
 export const get = async (req, res) => {
 
-	let documents = await queryCollection(Announcement, req.body);
+	let documents = await db.queryCollection(Announcement, req.body);
 
 	res.setHeader('Content-Type', 'application/json');
 	res.json({
@@ -15,7 +16,7 @@ export const get = async (req, res) => {
 
 export const post = async (req, res) => {
 	// TODO: Add auth protection
-	let document = await postCollection(Announcement, req.body);
+	let document = await db.postCollection(Announcement, req.body);
 	console.log(document);
 	res.setHeader('Content-Type', 'application/json');
 	res.json({
@@ -30,7 +31,7 @@ export const post = async (req, res) => {
 export const put = async (req, res) => {
 	// TODO: Add auth protection
 	let { query, delta } = req.body;
-	let document = await editCollection(Announcement, query, delta);
+	let document = await db.editCollection(Announcement, query, delta);
 	console.log(document);
 	res.setHeader('Content-Type', 'application/json');
 	res.json(document);
@@ -39,5 +40,5 @@ export const put = async (req, res) => {
 export const del = async (req, res, next) => {
 	// TODO: Add auth protection
 	res.setHeader('Content-Type', 'application/json');
-	res.json(await deleteCollection(Announcement, req.body));
+	res.json(await db.deleteCollection(Announcement, req.body));
 };
