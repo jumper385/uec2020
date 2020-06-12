2fe96626fd2c8e6ae2a6cfababa600a596e7b35f
Henry Chen <henrychen385@gmail.com>
2020-06-13 00:35:15 +0800

    auth: added login route

--- authapi/index.js
+++ authapi/index.js
@@ -6,9 +6,9 @@ const jwt = require('jsonwebtoken');
 const mongoose = require('mongoose');
 
 const User = require('./mongo/UserSchema');
-const Database = require('./mongo/dbinteractions');
+const db = require('./mongo/mongoosehelpers');
 
-const saltrounds = 30;
+const saltrounds = 10;
 const sampleHash = bcrypt.hash('st18chenh', saltrounds);
 const sampleUser = 'chenry';
 const app = express();	
@@ -22,17 +22,17 @@ app.get('/',(req,res) => {
 
 app.post('/profile', async (req,res) => {
 	try{
-		if(!req.body.password) throw Error('password is missing');
-		const findUser = User.find({$or:[
+		const findUser = await User.find({$or:[
 			{username:req.body.username},{email:req.body.email}
 		]});
-		const usersearch = await findUser;
-		if (usersearch.length == 0){
+
+		if (findUser.length < 1){
 			const newProfile = User.create({...req.body, password: await bcrypt.hash(req.body.password, saltrounds)});
 			res.json(await newProfile);
 		} else {
 			throw Error('User already exists');
 		}
+
 	} catch (err) {
 		console.log(err);
 		res.status(500).json({error:true,message:err.message});
@@ -40,11 +40,25 @@ app.post('/profile', async (req,res) => {
 });
 
 app.post('/login', async (req,res) => {
-	const {username, password} = req.body;
-	const userExists = username == sampleUser;
-	const correctlogin = await bcrypt.compare(password, await sampleHash);
+	try {
 
-	res.json(userExists && correctlogin ? {message:'Successful Login'} : {message: 'Login Failed...'});
+		const {username, password} = req.body;
+
+		const userquery = await db.queryCollection(User, {$or:[{username:username}, {email:username}]});
+		if (userquery.length < 1) res.status(401).json({error:true, message:'incorrect username...'});
+
+		const comparepwd = await bcrypt.compare(password, userquery[0].password);
+		if (!comparepwd) res.status(401).json({error:true, message:'incorrect login...'});
+		
+		if (comparepwd) {
+			let profile = userquery.map(({firstname, lastname, username, email}) => ({firstname, lastname, username, email}));
+			let token = jwt.sign({...profile[0]}, process.env.NODE_ENV === 'development' ? 'secretkey' : process.env.JWT_KEY);
+			res.status(200).json({message:'successful login', jwt:token});
+		}
+
+	} catch (err) {
+		res.status(500).json({error:true, message:err.message});
+	}
 });
 
 app.listen(3000, () => console.log('new connection'));
--- authapi/mongo/UserSchema.js
+++ authapi/mongo/UserSchema.js
@@ -5,6 +5,9 @@ const UserSchema = new Schema({
 	email:{type:String, required:true},
 	password:{type:String, required:true},
 	role:{type:String, required:true},
+	firstname:{type:String, required:true},
+	lastname:{type:String, required:true},
+	gender:{type:String, required:true},
 });
 
 module.exports = model('user',UserSchema);
