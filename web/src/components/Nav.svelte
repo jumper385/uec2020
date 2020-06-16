2c0bd88f2c4da58f6e86bfd9148b7dd3f1bd74de
Henry Chen <henrychen385@gmail.com>
2020-06-16 11:21:34 +0800

    made signin page look pretty

--- web/src/components/Nav.svelte
+++ web/src/components/Nav.svelte
@@ -1,141 +1,157 @@
 <script>
-	import { fade, fly } from 'svelte/transition';
-	import { goto, stores } from "@sapper/app";
-	const { session } = stores();
-	let banner = {}
-	let closed = false;
+  import { fade, fly } from "svelte/transition";
+  import { goto, stores } from "@sapper/app";
+  const { session } = stores();
+  let banner = {};
+  let closed = false;
 </script>
 
-<svelte:head>
-	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-</svelte:head>
-
 <style>
+  nav {
+    height: 36pt;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 0 16pt;
+    padding-top: 12pt;
+    margin-bottom: 12pt;
+  }
 
-	nav {
-		height: 36pt;
+  nav .logo {
+    height: 30pt;
+    margin-right: 32pt;
+  }
+
+  nav .link-holder {
+    margin: 0;
+    /* background:pink; */
+  }
+
+  nav .link {
+    color: rgb(0, 100, 200);
+    text-decoration: none;
+    padding: 0 6pt;
+    text-transform: capitalize;
+  }
+
+  nav .link:hover {
+    text-decoration: underline;
+  }
+
+  nav .link:visited {
+    color: rgb(0, 100, 150);
+  }
+
+  .banner {
+    background: #2ed573;
+    width: 100vw;
+    text-align: center;
+    font-weight: 600;
+    font-size: 10pt;
+    padding: 0 16pt;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    box-sizing: border-box;
+  }
+
+  .banner-close {
+    border-radius: 32pt;
+    font-size: 10pt;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    color: white;
+    cursor: pointer;
+  }
+
+  .link-agg {
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  button.link {
+    border: none;
+    font-size: 1em;
+    background: none;
+    color: rgb(0, 100, 200);
+    text-decoration: none;
+    text-transform: capitalize;
+    cursor: pointer;
+  }
+
+  @media (max-width: 500pt) {
+    nav {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      height: auto;
+    }
+
+	nav .link-agg {
 		display:flex;
-		align-items: center;
-		justify-content: space-between;
-		padding: 0 16pt;
-		padding-top:12pt;
-		margin-bottom: 12pt;
+		flex-direction: column;
 	}
 
-	nav .logo {
-		height:30pt;
-		margin-right:32pt;
-	}
+    nav .link {
+      font-size: 0.8em;
+    }
 
-	nav .link-holder {
-		margin:0;
-		/* background:pink; */
-	}
+    nav .logo {
+      margin-right: 0pt;
+    }
 
-	nav .link {
-		color: rgb(0,100,200);
-		text-decoration: none;
-		padding: 0 6pt;
-		text-transform: capitalize
-	}
-
-	nav .link:hover {
-		text-decoration: underline;
-	}
-
-	nav .link:visited {
-		color: rgb(0,100,150);
-	}
-
-	.banner {
-		background:#2ed573;
-		width:100vw;
-		text-align:center;
-		font-weight: 600;
-		font-size:10pt;
-		padding:0 16pt;
-		display: flex;
-		align-items: center;
-		justify-content: space-between;
-		box-sizing: border-box;
-	}
-
-	.banner-close{
-		border-radius:32pt;
-		font-size:10pt;
-		display:flex;
-		align-items: center;
-		justify-content: center;
-		color:white;
-		cursor: pointer;
-	}
-
-	.link-agg {
-		width:100%;
-		display:flex;
-		justify-content: space-between;
-		align-items: center;
-	}
-
-	button.link {
-		border:none;
-		font-size:1em;
-		background:none;
-		color: rgb(0,100,200);
-		text-decoration: none;
-		text-transform: capitalize;
-		cursor: pointer;
-	}
-
-	 @media (max-width: 500pt){
-		 nav {
-			 display:flex;
-			 flex-direction: column;
-			 align-items: center;
-			 height:auto;
-		 }
-
-		 nav .link {
-			 font-size: 0.8em;
-		 }
-
-		 nav .logo {
-			margin-right:0pt;
-		}
-
-		 nav .link-holder {
-			 margin-top:8pt;
-		 }
-	 }
-
+    nav .link-holder {
+      margin-top: 8pt;
+    }
+  }
 </style>
 
+<svelte:head>
+  <link
+    href="https://fonts.googleapis.com/icon?family=Material+Icons"
+    rel="stylesheet" />
+</svelte:head>
 <nav>
-	<a href='.'>
-		<img class='logo' src='logo96.png' alt='uec logo'/>
-	</a>
-	<div class='link-agg'>
-		<div class='link-holder'>
-			<a class='link' href='.'>home</a>
-			<a class='link' href='about'>about</a>
-			<a class='link' href='sponsors'>sponsors</a>
-			<a class='link' href='contact'>contact</a>
-		</div>
-		<div class='link-holder'>
-			{#if $session.token}
-			<a class='link' href='profile'>profile</a>
-			<button class='link' style='color:#ff4757' on:click={e => $session.token = null}>logout</button>
-			{:else}
-			<a class='link' href='login'>login</a>
-			<a class='link' href='signup'>signup</a>
-			{/if}
-		</div>
-	</div>
+  <a href=".">
+    <img class="logo" src="logo96.png" alt="uec logo" />
+  </a>
+  <div class="link-agg">
+    <div class="link-holder">
+      <a class="link" href=".">home</a>
+      <a class="link" href="about">about</a>
+      <a class="link" href="sponsors">sponsors</a>
+      <a class="link" href="contact">contact</a>
+    </div>
+    <div class="link-holder">
+      {#if $session.token}
+        <a class="link" href="profile">profile</a>
+        <button
+          class="link"
+          style="color:#ff4757"
+          on:click={(e) => ($session.token = null)}>
+          logout
+        </button>
+      {:else}
+        <a class="link" href="login">login</a>
+        <a class="link" href="signup">signup</a>
+      {/if}
+    </div>
+  </div>
 </nav>
 
 {#if !closed}
-<div transition:fade="{{duration:120}}" class='banner' style={banner.message ? null : 'display: None'}>
-<p style='color:white; text-align:left;'>{banner.message ? null : 'display: None'}</p>
-<span on:click={() => closed = true} class="banner-close material-icons"> check_box </span>
-</div>
+  <div
+    transition:fade={{ duration: 120 }}
+    class="banner"
+    style={banner.message ? null : 'display: None'}>
+    <p style="color:white; text-align:left;">
+      {banner.message ? null : 'display: None'}
+    </p>
+    <span on:click={() => (closed = true)} class="banner-close material-icons">
+      check_box
+    </span>
+  </div>
 {/if}
--- web/src/routes/login/index.svelte
+++ web/src/routes/login/index.svelte
@@ -6,6 +6,9 @@
     password = null;
   $: button = "Login";
 
+  let errorcount = 0;
+  $: success = !!$session.token;
+
   const onSubmit = async (e) => {
     let response = await fetch(`apiv1/auth/login`, {
       method: "POST",
@@ -17,65 +20,108 @@
         password: password,
       }),
     });
+
     if (response.status == 201) {
       $session.token = await response.json();
       button = $session.token && "Success!";
+      success = $session.token && true;
       goto("/profile");
     }
 
-    if (response.status != 201) button = "Wrong Password - try again";
+    if (response.status != 201) {
+      button = "Try Again...";
+      errorcount += 1;
+    }
   };
 </script>
 
-<svelte:head>
-  <title>Login</title>
-</svelte:head>
-
 <style>
   .container {
-    width:250pt;
-    margin:0 auto;
+    width: 250pt;
+    margin: 0 auto;
   }
 
   .container h1 {
-    text-align:center;
+    text-align: center;
   }
 
   .container .inputbutton {
     -webkit-appearance: none;
-    width:100%;
-    border:0;
+    width: 100%;
+    border: 0;
     padding: 8pt 0;
     background: #fcd703;
     border-radius: 6pt;
+    margin-top: 8pt;
+  }
+
+  .container .inputbutton:disabled {
+    background: #fff8cf;
+    color: rgba(0, 0, 0, 0.36);
+  }
+
+  .container .loginerror {
+    background: #ff4757;
+    color: white;
   }
 
   .inputcontainer .input_title {
-    text-align:left;
-    margin:0;
-    margin-left:6pt;
+    text-align: left;
+    margin: 0;
+    font-size: 1em;
+    margin-left: 6pt;
+    margin-bottom: 3pt;
+    color: rgba(0, 0, 0, 0.36);
+    text-transform: lowercase;
   }
 
   .inputcontainer .textinput {
     -webkit-appearance: none;
-    width:100%;
-    box-sizing:border-box;
-    margin-bottom:8pt;
+    width: 100%;
+    box-sizing: border-box;
+    margin-bottom: 8pt;
+    padding: 6pt;
+    border: none;
+    border-radius: 6pt;
+    border: rgba(0, 0, 0, 0.12) solid 1pt;
   }
-
 </style>
 
-<div class='container'>
+<svelte:head>
+  <title>Login</title>
+</svelte:head>
+<div class="container">
   <h1>Login</h1>
+
   <form on:submit|preventDefault={onSubmit}>
-    <div class='inputcontainer'>
-      <p class='input_title'>Username</p>
-      <input class='textinput' bind:value={username} name="username" />
+
+    <div class="inputcontainer">
+      <p class="input_title">Username</p>
+      <input class="textinput" bind:value={username} name="username" />
     </div>
-    <div class='inputcontainer'>
-      <p class='input_title'>Password</p>
-      <input class='textinput' bind:value={password} type="password" name="password" />
+
+    <div class="inputcontainer">
+      <p class="input_title">Password</p>
+      <input
+        class="textinput"
+        bind:value={password}
+        type="password"
+        name="password" />
     </div>
-    <input class='inputbutton' type="submit" bind:value={button} />
+    {#if errorcount == 0}
+      <input
+        disabled={!username}
+        class="inputbutton"
+        type="submit"
+        style={success && 'color:#7bed9f'}
+        bind:value={button} />
+    {:else}
+      <input
+        disabled={!username}
+        class="inputbutton loginerror"
+        type="submit"
+        bind:value={button} />
+    {/if}
   </form>
+
 </div>
